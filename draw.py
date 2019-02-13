d4ee4a8f1efd6fdcd13dcbf7ec628c76bb1b2699
Johannes Boczek <johannes.boczek@web.de>
2019-02-13 23:50:00 +0100

    Rename function

--- draw.py
+++ draw.py
@@ -4,7 +4,7 @@ boardTexture = Image.open("assets/board.png");
 stoneBlack = Image.open("assets/stone_black.png");
 stoneWhite = Image.open("assets/stone_white.png");
 
-def loop_image(width, height, texture):
+def draw_loop_image(width, height, texture):
 	"""Create an image of given size using an arbitrary texture.
 	The texture will loop if smaller than the image size.
 	
