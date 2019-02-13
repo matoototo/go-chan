919765032932d830f5c9ba41636ffbbae0933f14
matoototo <furrowedsip@gmail.com>
2019-02-13 23:37:45 +0100

    Add Challenge class

--- game.py
+++ game.py
@@ -4,3 +4,11 @@ class Game:
     def __init__ (self, size, unique):
         self.size = size
         self.unique = unique
+
+class Challenge:
+    def __init__ (self, challenger, challenged, boardSize):
+        self.challenger = challenger
+        self.challenged = challenged
+        self.boardSize = boardSize
+    def __copy__ (self):
+        return Challenge(self.challenger, self.challenged, self.boardSize)
