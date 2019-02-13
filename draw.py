e71519c0c560befb555eed4a953f9484fa771c71
Johannes Boczek <johannes.boczek@web.de>
2019-02-13 21:18:56 +0100

    Add draw_board function to draw goban lines

--- draw.py
+++ draw.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 boardTexture = Image.open("assets/board.png");
 stoneBlack = Image.open("assets/stone_black.png");
@@ -23,5 +23,33 @@ def loop_image(width, height, texture):
 	
 	return board
 
-#b = create_board(500, 500, boardTexture)
-#b.save("test.png")
+def draw_board(image, boardSize):
+	"""Draw goban lines on the specified image.
+	Returns a new copy instead of altering the original image.
+	
+	Arguments:
+	image -- The image to draw on
+	boardSize -- The size of the board (9, 13, or 19)
+	"""
+	
+	newImage = image.copy()
+	imageWidth, imageHeight = newImage.size
+	draw = ImageDraw.Draw(newImage)
+	stepX = imageWidth / boardSize;
+	stepY = imageHeight / boardSize;
+	
+	for i in range(1, boardSize):
+		x = stepX * i;
+		
+		draw.line([(x, 0), (x, imageHeight)])
+	
+	for i in range(1, boardSize):
+		y = stepY * i;
+		
+		draw.line([(0, y), (imageWidth, y)])
+	
+	return newImage
+
+#board = loop_image(500, 500, boardTexture);
+#board = draw_board(board, 19)
+#board.save("test.png")
