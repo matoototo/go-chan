c2af016d903505de745ae433a6c2eccf7529bedd
Johannes Boczek <johannes.boczek@web.de>
2019-02-13 21:02:43 +0100

    Improve docstring and rename function

--- draw.py
+++ draw.py
@@ -4,8 +4,15 @@ boardTexture = Image.open("assets/board.png");
 stoneBlack = Image.open("assets/stone_black.png");
 stoneWhite = Image.open("assets/stone_white.png");
 
-def create_board(width, height, texture):
-	"""Create an image of a board of given size using an arbitrary texture"""
+def loop_image(width, height, texture):
+	"""Create an image of given size using an arbitrary texture.
+	The texture will loop if smaller than the image size.
+	
+	Arguments:
+	width -- The width of the resulting image
+	height -- The height of the resulting image
+	texture -- The texture to use
+	"""
 	
 	board = Image.new("RGBA", (width, height), (255, 255, 255, 255))
 	textureWidth, textureHeight = texture.size
