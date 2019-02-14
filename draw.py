942d3f274a48ed02f1b7e71705271db95d0e73d7
Johannes Boczek <johannes.boczek@web.de>
2019-02-14 10:12:40 +0100

    Implement stone drawing
    Tweak some of the rendering code in general

--- draw.py
+++ draw.py
@@ -4,9 +4,9 @@ BOARD_TEXTURE = Image.open("assets/board.png")
 STONE_BLACK_TEXTURE = Image.open("assets/stone_black.png")
 STONE_WHITE_TEXTURE = Image.open("assets/stone_white.png")
 
-BOARD_SETTINGS_9 = (8, False, 2)
-BOARD_SETTINGS_13 = (12, False, 3)
-BOARD_SETTINGS_19 = (18, True, 3)
+BOARD_SETTINGS_9 = (9, False, 2)
+BOARD_SETTINGS_13 = (13, False, 3)
+BOARD_SETTINGS_19 = (19, True, 3)
 
 class VisualBoard:
 	"""Contains data for the visual representation of a Go board."""
@@ -26,11 +26,18 @@ class VisualBoard:
 		
 		self.width = width
 		self.height = height
+		self.innerWidth = width - borderSize * 2
+		self.innerHeight = height - borderSize * 2
 		self.borderSize = borderSize
 		self.settings = settings
-		self.stoneBlackTexture = stoneBlackTexture
-		self.stoneWhiteTexture = stoneWhiteTexture
 		self.baseImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+		
+		(boardSize, _, _) = self.settings
+		stoneWidth = int(self.innerWidth / boardSize)
+		stoneHeight = int(self.innerWidth / boardSize)
+		
+		self.stoneBlackTexture = stoneBlackTexture.resize((stoneWidth, stoneHeight), Image.BICUBIC)
+		self.stoneWhiteTexture = stoneWhiteTexture.resize((stoneWidth, stoneHeight), Image.BICUBIC)
 		
 		self.__draw_board_texture(boardTexture)
 		self.__draw_board()
@@ -50,17 +57,17 @@ class VisualBoard:
 		"""Draw goban lines, labels and star points on the internal image."""
 		
 		COLOR = (0, 0, 0, 200)
-		STAR_POINT_SIZE = 2
-		LABEL_BOARD_SPACING = 10
+		LINE_WIDTH = 2
+		STAR_POINT_SIZE = 4
+		FONT_SIZE = 18
 		
 		(boardSize, drawExtraStarPoints, starPointOffset) = self.settings
-		imageWidth, imageHeight = self.baseImage.size
-		innerWidth = imageWidth - self.borderSize * 2
-		innerHeight = imageHeight - self.borderSize * 2
+		boardSize -= 1
+		stepX = self.innerWidth / boardSize
+		stepY = self.innerHeight / boardSize
+		labelBoardSpacing = self.borderSize / 3
 		draw = ImageDraw.Draw(self.baseImage)
-		stepX = innerWidth / boardSize
-		stepY = innerHeight / boardSize
-		font = ImageFont.truetype("assets/font_fifteentwenty.otf", 12)
+		font = ImageFont.truetype("assets/font_fifteentwenty.otf", FONT_SIZE)
 		
 		# Draw lines and labels
 		for i in range(0, boardSize + 1):
@@ -68,18 +75,18 @@ class VisualBoard:
 			label = chr(ord('A') + i)
 			labelWidth, labelHeight = draw.textsize(label, font)
 			
-			draw.line([(x, self.borderSize), (x, innerHeight + self.borderSize)], COLOR)
-			draw.text((x - labelWidth / 2, self.borderSize - labelHeight - LABEL_BOARD_SPACING), label, COLOR, font)
-			draw.text((x - labelWidth / 2, self.borderSize + innerHeight + LABEL_BOARD_SPACING), label, COLOR, font)
+			draw.line([(x, self.borderSize), (x, self.innerHeight + self.borderSize)], COLOR, LINE_WIDTH)
+			draw.text((x - labelWidth / 2, self.borderSize - labelHeight - labelBoardSpacing), label, COLOR, font)
+			draw.text((x - labelWidth / 2, self.borderSize + self.innerHeight + labelBoardSpacing), label, COLOR, font)
 		
 		for i in range(0, boardSize + 1):
 			y = self.borderSize + stepY * i
 			label = str(boardSize - i + 1)
 			labelWidth, labelHeight = draw.textsize(label, font)
 			
-			draw.line([(self.borderSize, y), (innerWidth + self.borderSize, y)], COLOR)
-			draw.text((self.borderSize - labelWidth - LABEL_BOARD_SPACING, y - labelHeight / 2), label, COLOR, font)
-			draw.text((self.borderSize + innerWidth + LABEL_BOARD_SPACING, y - labelHeight / 2), label, COLOR, font)
+			draw.line([(self.borderSize, y), (self.innerWidth + self.borderSize, y)], COLOR, LINE_WIDTH)
+			draw.text((self.borderSize - labelWidth - labelBoardSpacing, y - labelHeight / 2), label, COLOR, font)
+			draw.text((self.borderSize + self.innerWidth + labelBoardSpacing, y - labelHeight / 2), label, COLOR, font)
 		
 		# Calculate star point positions
 		centerX = boardSize / 2 * stepX + self.borderSize
@@ -110,7 +117,51 @@ class VisualBoard:
 		stones -- A 2D-array of values either 0 (no stone), 1 (black stone) or 2 (white stone)
 		"""
 		
-		pass
+		(boardSize, _, _) = self.settings
+		boardSize -= 1
+		newImage = self.baseImage.copy()
+		stepX = self.innerWidth / boardSize
+		stepY = self.innerHeight / boardSize
+		
+		for y, row in enumerate(stones):
+			for x, stone in enumerate(row):
+				posX = int(self.borderSize + x * stepX)
+				posY = int(self.borderSize + y * stepY)
+				image = None
+				
+				if stone == 1:
+					image = self.stoneBlackTexture
+				elif stone == 2:
+					image = self.stoneWhiteTexture
+				else:
+					image = Image.new("RGBA", (1, 1))
+				
+				imageWidth, imageHeight = image.size
+				
+				newImage.paste(image, (int(posX - imageWidth / 2), int(posY - imageHeight / 2)), image)
+		
+		return newImage
 
-#board = VisualBoard(500, 500, 50, BOARD_SETTINGS_19, BOARD_TEXTURE, STONE_BLACK_TEXTURE, STONE_WHITE_TEXTURE)
-#board.baseImage.save("test.png")
+#board = VisualBoard(750, 750, 75, BOARD_SETTINGS_19, BOARD_TEXTURE, STONE_BLACK_TEXTURE, STONE_WHITE_TEXTURE)
+#stones = [
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+#	[0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#]
+#board.generate_image(stones).save("test.png")
